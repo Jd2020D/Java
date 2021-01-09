801d6106260584119391ee3285b08f26a22df367
Jd2020D <jed19of@gmail.com>
2021-01-09 17:41:16 +0200

    Spring Data 1 , Update and Delete

---
+++ Java Spring/SpringData1/update/src/main/java/com/axsos/update/services/BookService.java
@@ -0,0 +1,55 @@
+package com.axsos.update.services;
+
+import java.util.List;
+import java.util.Optional;
+
+//...
+import org.springframework.stereotype.Service;
+import com.axsos.update.models.Book;
+import com.axsos.update.repositories.BookRepository;
+@Service
+public class BookService {
+ // adding the book repository as a dependency
+ private final BookRepository bookRepository;
+ 
+ public BookService(BookRepository bookRepository) {
+     this.bookRepository = bookRepository;
+ }
+ // returns all the books
+ public List<Book> allBooks() {
+     return bookRepository.findAll();	
+ }
+ // creates a book
+ public Book createBook(Book b) {
+     return bookRepository.save(b);
+ }
+ // retrieves a book
+ public Book findBook(Long id) {
+     Optional<Book> optionalBook = bookRepository.findById(id);
+     if(optionalBook.isPresent()) {
+         return optionalBook.get();
+     } else {
+         return null;
+     }
+ }
+ public Book updateBook(Long id ,String title, String desc, String lang, int pages) {
+	 Book book=this.findBook(id);
+	 if (book!= null) {
+		 book.setTitle(title);
+		 book.setDescription(desc);
+		 book.setLanguage(lang);
+		 book.setNumberOfPages(pages);
+		 bookRepository.save(book);
+	     return book;
+	 }
+	 return null;
+		 
+ }
+ 
+ public void deleteBook(Long id) {
+	 try {
+		 bookRepository.deleteById(id);
+	 }catch(Exception e) {
+	 }
+ }
+}
