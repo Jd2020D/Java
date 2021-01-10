45b360285fcd6f469f44159594698fe14c5b3951
Jd2020D <jed19of@gmail.com>
2021-01-10 05:30:58 +0200

    add /books/new form to create the book

--- Java Spring/SpringData1/update/src/main/java/com/axsos/update/controllers/BooksController.java
+++ Java Spring/SpringData1/update/src/main/java/com/axsos/update/controllers/BooksController.java
@@ -2,9 +2,15 @@ package com.axsos.update.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.axsos.update.models.Book;
 import com.axsos.update.services.BookService;
@@ -23,4 +29,25 @@ public class BooksController {
      model.addAttribute("books", books);
      return "/books/index.jsp";
  }
+ @RequestMapping("/books/{id}")
+ public String show(@PathVariable Long id ,Model model) {
+	 Book book=bookService.findBook(id);
+     model.addAttribute("book", book);
+     return "/books/show.jsp";
+ }
+
+ @RequestMapping("/books/new")
+ public String newBook(@ModelAttribute("book") Book book) {
+     return "/books/new.jsp";
+ }
+ @RequestMapping(value="/books", method=RequestMethod.POST)
+ public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+     if (result.hasErrors()) {
+         return "/books/new.jsp";
+     } else {
+         bookService.createBook(book);
+         return "redirect:/books";
+     }
+ }
+
 }
